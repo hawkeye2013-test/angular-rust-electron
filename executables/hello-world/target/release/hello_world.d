9003816fe07feb25209be2f8bcde2ac800ecbfde
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2020-08-08 12:56:13 -0400

    ignore build directory in rust

--- executables/hello-world/target/release/hello_world.d
+++
@@ -1,1 +0,0 @@
-D:\code\angular-rust-electron\executables\hello-world\target\release\hello_world.exe: D:\code\angular-rust-electron\executables\hello-world\src\main.rs
--- executables/hello-world/target/rls/debug/deps/hello_world-3378fd0ae446046b.d
+++
@@ -1,5 +0,0 @@
-d:\code\angular-rust-electron\executables\hello-world\target\rls\debug\deps\hello_world-3378fd0ae446046b.rmeta: src\main.rs
-
-d:\code\angular-rust-electron\executables\hello-world\target\rls\debug\deps\hello_world-3378fd0ae446046b.d: src\main.rs
-
-src\main.rs:
